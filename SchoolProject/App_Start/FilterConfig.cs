b01c9f793c1a1c30367e9822886e60261acb1405
televich <belarusso@mail.ru>
2017-03-13 19:57:31 +0300

    create solution

---
+++ SchoolProject/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolProject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            // start project
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
